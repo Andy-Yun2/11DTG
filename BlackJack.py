5fd20409d01342c4cf77fc937b1c8474007d2196
Andy-Yun2 <196586@cloud.rangitoto.school.nz>
2025-08-26 10:00:19 +1200

    black jack upload

---
+++ BlackJack.py
@@ -0,0 +1,60 @@
+import random as r
+total = 0
+dtotal = 0
+
+card_number = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q","K", "A"]
+deck = [x for _ in range(4) for x in [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q","K", "A"]]
+
+player = []
+dealer = []
+
+player.append(r.choice(card_number))
+player.append(r.choice(card_number))
+print(player)
+
+another_one = input("Get another card? ").lower()
+if another_one == "y":
+    player.append(r.choice(card_number))
+else:
+    pass
+
+for i in player:
+    if i == "K" or i == "Q" or i == "J":
+        total += 10
+
+    elif i == "A":
+        total += 11
+    else:
+        total += i
+
+dealer.append(r.choice(card_number))
+dealer.append(r.choice(card_number))
+for i in dealer:
+    if i == "K" or i == "Q" or i == "J":
+        dtotal += 10
+    elif i == "A":
+        dtotal += 11
+    else:
+        dtotal += i
+if dtotal < 18:
+    dealer.append(r.choice(card_number))
+    dtotal = 0
+    for i in dealer:
+        if i == "K" or i == "Q" or i == "J":
+            dtotal += 10
+        elif i == "A":
+            dtotal += 11
+        else:
+            dtotal += i
+print(total)
+if dtotal > 21:
+    print("the dealer busts, you win!")
+else:
+
+
+if total > 21:
+    print("Bust! You lost")
+else:
+
+
+
--- RPS.py
+++ RPS.py
@@ -1,45 +1,31 @@
 import random as r
 
 choices = [1, 2, 3]
-ai_options = 0
+ai_options = r.choice(choices)
 player_options = 0
-
-rules = ["Rock(🪨) beats Scissors(✂️)",
-         "Scissor(✂️) beats Paper(📄)",
-         "Paper(📄) beats Rock(🪨)"]
-print("----------‼️Welcome to Rock Paper Scissor‼️----------")
-print("Rules:")
-for i in rules:
-    print(i)
-print("------------------------------------------------------")
-
-def compare_choices():
-    if player_options == ai_options:
-        return "Draw"
-    elif player_options == 3 and ai_options == 2 :
-        return "Lose"
-    elif player_options == 2 and ai_options == 1:
-        return "Lose"
-    elif player_options == 1 and ai_options == 3:
-        return "Lose"
-    else:
-        return "Win"
-
-while True:
+valid = True
+while valid:
     try:
         player_options = input("Select your choice: (type 1 for 🪨)(type 2 for ✂️)(type 3 for 📄): ")
         player_options = int(player_options)
-        if player_options > 3 or player_options < 0:
+        if player_options > 3 or player_options <= 0:
             raise ValueError
-        break
+        valid = False
     except ValueError:
-        print("I don't see that choice 🤨")
-        print("Please re-enter your choice.")
-print("Your Opponent is choosing...")
-ai_options = r.choice(choices)
-print(f"Your opponent chose {ai_options}")
-result = compare_choices()
-print(f"You {result}")
+        print("Invalid")
+
+if player_options == ai_options:
+    print("Draw")
+elif player_options == 3 and ai_options == 2 :
+    print("Lose")
+elif player_options == 2 and ai_options == 1:
+    print("Lose")
+elif player_options == 1 and ai_options == 3:
+    print("Lose")
+else:
+    print("Win")
+
+
 
 
 
