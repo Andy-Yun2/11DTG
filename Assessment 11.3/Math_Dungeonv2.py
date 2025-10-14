5fd263f8c2dbb734834e0c7029eec6a8f23679cd
nD-Creation <yunyouyu1222@gmail.com>
2025-10-15 10:04:51 +1300

    Made output question with answering check

--- Assessment 11.3/Math_Dungeonv2.py
+++ Assessment 11.3/Math_Dungeonv2.py
@@ -51,19 +51,17 @@ class Math:
         self.question_text, self.answer = self.generate_question()
 
 
-
     def generate_question(self):
-        match self.q_type:
-            case "numbers":
-                return self.generate_numbers()
-            case "geometry":
-                return self.generate_geometry()
-            case "graphing":
-                return self.generate_graphing()
-            case "algebra":
-                return self.generate_algebra()
-            case _:
-                return None
+        if self.q_type == "numbers":
+            return self.generate_numbers()
+        elif self.q_type == "geometry":
+            return self.generate_geometry()
+        elif self.q_type == "graphing":
+            return self.generate_graphing()
+        elif self.q_type == "algebra":
+            return self.generate_algebra()
+        else:
+            return None
 
     def generate_numbers(self):
         if self.level < 2:
@@ -103,12 +101,16 @@ class Math:
 
 
 
+question = Math(6,4,"geometry")
+print("Question: ", question.question_text)
 
+user_answer = float(input("Your answer: "))
 
-
-
-
-
+if user_answer == question.answer:
+    print("Correct")
+else:
+    print("WRONG")
+    print("The right answer was:", question.answer)
 
 
 
